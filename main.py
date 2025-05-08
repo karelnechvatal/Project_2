bfec8529db15d806053332ab82df5c0911e25ad8
karelnechvatal <knechvatal6@seznam.cz>
2025-05-08 14:51:30 +0200

    Přidání kontroly vstupu od hráče

--- main.py
+++ main.py
@@ -38,3 +38,20 @@ def print_board(board):
         print(f"| {board[i]} | {board[i+1]} | {board[i+2]} |")
     print("+---+---+---+")
     print("=" * 40)
+
+# Kontrola vstupu od hráče
+def get_player_input(board, player_symbol):
+    while True:
+        try:
+            move = input(f"Player {player_symbol} | Please enter your move number (1-9): ")
+            position = int(move) - 1
+            if position < 0 or position >= 9:
+                print("Invalid move: Please choose a number from 1 to 9.")
+            elif board[position] != EMPTY_CELL:
+                print("Invalid move: This position is already taken.")
+            else:
+                return position
+        except ValueError:
+            print("Invalid input: Please enter a valid number from 1 to 9.")
+2
+
